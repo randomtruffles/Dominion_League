e451ba75e9476f1bd01a57a0e7d066a0ef5d9072
truffles <truffles.dominion@gmail.com>
2020-08-26 15:38:04 -0400

    ssounder <- turbobaseball

--- parsing/scrap.py
+++ parsing/scrap.py
@@ -1,13 +1,29 @@
 import json
-with open('../_data/draft_season_41_players.json') as file:
-    players = json.load(file)
+with open('../_data/friendly_league_history.json') as file:
+    seasons = json.load(file)["seasons"]
 
-playing = {}
-for p in players:
-    playing[p["Name"]] = "41"
+min_win = 30
+names = []
+seasons_occur = []
 
+for season in seasons:
+    for division in seasons[season]:
+        if division == "E12" and season == "29":
+            pass
+        elif division != "champion":
+            for member in seasons[season][division]["members"]:
+                member_data = seasons[season][division]["members"][member]
+                if member_data["rank"] == 1 and (float(member_data["wins"]) + float(member_data["losses"]) >= 25.0):
+                    if float(member_data["wins"]) < min_win and float(member_data["wins"]) >= 10.0 :
+                        min_win = float(member_data["wins"])
+                        names = [member]
+                        seasons_occur = [season]
+                    elif float(member_data["wins"]) == min_win:
+                        names.append(member)
+                        seasons_occur.append(season)
 
-file = "../_data/season_41_players.json"
-with open(file, 'w') as filetowrite:
-    # convert python dictionary to json and write it
-    json.dump(playing, filetowrite)
+print("Lowest Win with Rank 1: " + str(min_win))
+print("Players:")
+print(names)
+print("Seasons Occured:")
+print(seasons_occur)
