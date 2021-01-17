d50161419193ab15d2f38e1740d58c88c62283b7
Random Truffles <66284902+randomtruffles@users.noreply.github.com>
2021-01-17 16:41:28 -0500

    dir

--- scripts/google-sheets.py
+++ scripts/google-sheets.py
@@ -22,8 +22,8 @@ def main():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists('./scripts/token.pickle'):
+        with open('./scripts/token.pickle', 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -34,7 +34,7 @@ def main():
                 './scripts/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open('./scripts/token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('sheets', 'v4', credentials=creds)
