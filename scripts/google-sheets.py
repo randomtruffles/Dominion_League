ef086e3377d938656313b8b9715477e980ee9719
Random Truffles <66284902+randomtruffles@users.noreply.github.com>
2021-01-18 01:22:10 -0500

    added cron job to scrape sheets every 5 minutes

--- scripts/google-sheets.py
+++ scripts/google-sheets.py
@@ -9,15 +9,27 @@ from google.auth.transport.requests import Request
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
-SAMPLE_RANGE_NAME = 'Class Data!A2:E'
+# Various sheets pages
+MONITORING_SHEET = '1CIvRxhLL2XrwR0eQDt2FWhEgN1twtl6_r7BmHdFQeEE'
+RESULTS = 'Condensed Results!A2:K'
+def assign_color(value):
+    value = int(value)
+    gradient = ["E77B72", "E88372", "EA8C71", "EC956F", "EF9E6E", "F2A76D", "F4B06B", "F7B96B", "F9C269", "FCCB67", "FED467", "F2D467", "E2D26B", "D0CF6F", "C0CC73", "AFCA76", "9EC77A", "8CC47E", "7CC181", "6DBF84", "5BBC88"]
+    steps = len(gradient)
+    range = 101
+    def bgcolor(color):
+        return "#{}".format(color)
+
+    return bgcolor(gradient[(value*steps)//101])
+
+def fpct(pct):
+    return "{0:.0%}".format(pct)
 
 def main():
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
-    testjson = {}
+    curr_season = {}
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -41,21 +53,83 @@ def main():
 
     # Call the Sheets API
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
+    print(f"Getting the information standings...")
+    result = sheet.values().get(spreadsheetId=MONITORING_SHEET,
+                                range=RESULTS).execute()
     values = result.get('values', [])
 
     if not values:
         print('No data found.')
     else:
-        print('Name, Major:')
+        division_info = []
         for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[4]))
-            testjson[row[0]] = row[4]
-    file = 'test.json'
+            cols = len(row)
+            if cols >= 7:
+                division = row[0]
+                players = row[1].split(',')
+                pcts = row[2].split(',')
+                wins = row[3].split(',')
+                losses = row[4].split(',')
+                ranks = row[5].split(',')
+                p1s = row[6].split(',')
+                p2s = row[7].split(',')
+                wins1s = row[8].split(',')
+                wins2s = row[9].split(',')
+                late_drops = []
+                if cols >= 11:
+                    late_drops = row[10].split(',')
+                    print(f"Getting the results for {division} with late drops: {late_drops}...")
+                else:
+                    print(f"Getting the results for {division}...")
+
+                # Match results
+                for idx in range(len(p1s)):
+                    p1 = p1s[idx]
+                    p2 = p2s[idx]
+                    wins1 = wins1s[idx]
+                    wins2 = wins2s[idx]
+                    result = {"player1":p1, "player2":p2, "wins1":wins1, "wins2":wins2}
+                    if division not in curr_season:
+                        curr_season[division]={}
+                        curr_season["name"] = division
+                        curr_season["tier"] = division[0]
+                        curr_season[division]["results"] = []
+                        curr_season[division]["by_player"] = {}
+                    if p1 not in curr_season[division]["by_player"]:
+                        curr_season[division]["by_player"][p1] = {}
+                    if p2 not in curr_season[division]["by_player"]:
+                        curr_season[division]["by_player"][p2] = {}
+                    curr_season[division]["results"].append(result)
+                    curr_season[division]["by_player"][p1][p2] = {"wins": wins1, "losses": wins2}
+                    curr_season[division]["by_player"][p2][p1] = {"wins": wins2, "losses": wins1}
+
+                # Member standings
+                curr_season[division]["members"] = {}
+                for p_idx, player in enumerate(players):
+                    if player == "":
+                        break
+                    color = assign_color(float(pcts[p_idx][:-1]))
+                    drop = "Yes" if player in late_drops else "No"
+                    curr_season[division]["members"][player] = {"name": player, "rank":int(ranks[p_idx]), "wins":wins[p_idx], "losses":losses[p_idx], "pct": pcts[p_idx], "color": color, "drop":drop}
+
+
+                # Tiebreaker values
+                for player in players:
+                    if player == "" or player in late_drops:
+                        break
+                    for opponent in players:
+                        if opponent == "":
+                            break
+                        if player != opponent and opponent not in late_drops and opponent in curr_season[division]["by_player"][player]:
+                            if curr_season[division]["members"][player]["pct"] == curr_season[division]["members"][opponent]["pct"]:
+                                if "tiebreaker" in curr_season[division]["members"][player]:
+                                    curr_season[division]["members"][player]["tiebreaker"] += curr_season[division]["by_player"][player][opponent]["wins"]
+                                else:
+                                    curr_season[division]["members"][player]["tiebreaker"] = curr_season[division]["by_player"][player][opponent]["wins"]
+    print("writing to file")
+    file = './_data/current_season.json'
     with open(file, 'w') as filetowrite:
-        json.dump(testjson, filetowrite)
+        json.dump(curr_season, filetowrite)
 
 
 if __name__ == '__main__':
