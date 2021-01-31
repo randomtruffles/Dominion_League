814a640d53ced586f72a3209489b89ce12e06040
Random Truffles <66284902+randomtruffles@users.noreply.github.com>
2021-01-31 18:28:17 -0500

    remove unidecode

--- scripts/fetch-current-season.py
+++ scripts/fetch-current-season.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import pickle
 import os.path
 import json
-from unidecode import unidecode
+#from unidecode import unidecode
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
